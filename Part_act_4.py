4a1555d590498c9a154ca556a9e316b20d599beb
gamath99 <gamamathurin1@gmail.com>
2026-06-30 22:45:25 -0400

    unit test 1 for the 6 sides

---
+++ Part_act_4.py
@@ -0,0 +1,31 @@
+""" 
+Dice Game 
+Gama Mathurin
+Cretion  of a dice game that contain 6 sides, another one with 10 sides and a last one with 20 sides, which should be roll 10 times for each. 
+6/30/2026
+"""
+
+#import the random module that would be use whenthe dice will roll
+import random
+
+#Create the die class
+class die:
+    """initialize the dice with a default of 6 sides """
+    def __init__(self, sides = 6):
+        self.sides = sides
+
+
+    """create the method that will be useto roll the die"""
+    def roll_die(self):
+        print(random.randint(1,self.sides))
+
+#Create the 6 sided dice
+print("Rolling of a 6-sided die 10 times:")
+die_6side = die()
+
+#Launch of the 6 sided die
+ 
+for i in range(10):
+    die_6side.roll_die()
+
+    
